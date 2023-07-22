9017ee4e1baafe2e3330726008f3f5813e61327e
caleb-oconnor <caleb.oconnor2@gmail.com>
2023-07-22 17:47:33 -0500

    v1.0.0 - Module set for release

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup
+
+from src import __version__, __author__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='MedicalImageConverter',
+    author=__author__,
+    author_email='csoconnor@mdanderson.org',
+    version=__version__,
+    description='Reads in medical images and converts them into numpy arrays.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={'MedicalImageConverter': 'src'},
+    packages=['MedicalImageConverter'],
+    include_package_data=True,
+    url='https://github.com/caleb-oconnor/MedicalImageConverter',
+    install_requires=required,
+)
