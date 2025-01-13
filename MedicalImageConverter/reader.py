dfe83043c3c8e491d4d9b16fe15e4c957de3a776
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-01-12 21:58:11 -0600

    Added - comments

--- MedicalImageConverter/reader.py
+++ MedicalImageConverter/reader.py
@@ -1,30 +1,98 @@
+"""
+Morfeus lab
+The University of Texas
+MD Anderson Cancer Center
+Author - Caleb O'Connor
+Email - csoconnor@mdanderson.org
 
-from parsar import file_parsar
-from ReadClasses.dicom import DicomReader
+
+Description:
+    Currently, reads in dicom files for modalities: CT, MR, DX, MG, US, RTSTRUCTS.
+
+    The user inputs either a given folder path (can contain multiple images and subfolders). The files are sorted into
+    separate images.
+
+    Other user input options:
+        file_list - if the user already has the files wanted to read in, must be in type list
+        exclude_files - if the user wants to not read certain files
+        only_tags - does not read in the pixel array just the tags
+        only_modality - specify which modalities to read in, if not then all modalities will be read
+        only_load_roi_names - will only load rois with input name, list format
+
+Functions:
+    read_dicoms - Reads in all dicom files and separates them into the image list variable
+
+"""
+
+from .parsar import file_parsar
+from .ReadClasses import DicomReader
 
 
 class Reader:
-    def __init__(self, folder_path=None, file_list=None, exclude_files=None):
+    """
+    Currently, reads in dicom files for modalities: CT, MR, DX, MG, US, RTSTRUCTS.
+
+    The user inputs either a given folder path (can contain multiple images and subfolders). The files are sorted into
+    separate images.
+
+    Other user input options:
+        file_list - if the user already has the files wanted to read in, must be in type list
+        existing_images - if the user wants to not read in certain images that may already have been read in
+        only_tags - does not read in the pixel array just the tags
+        only_modality - specify which modalities to read in, if not then all modalities will be read
+        only_load_roi_names - will only load rois with input name, list format
+    """
+    def __init__(self, folder_path=None, file_list=None, exclude_files=None, only_tags=False, only_modality=None,
+                 only_load_roi_names=None):
+        """
+        User must input either folder_path or file_list, this will be used to determine which files to read in.
+        If folder_path is input the "file_parsar" function runs to sort all the files into a dictionary of:
+        Dicom, MHD, Raw, Stl, 3mf
+
+        :param folder_path: single folder path
+        :type folder_path: string path
+        :param file_list: instead of folder_path a file_list can be given if the user already has the files to read in
+        :type file_list: list
+        :param exclude_files: if the user wants to not read certain files
+        :type exclude_files: list of file
+        :param only_tags: does not read in the pixel array just the tags
+        :type only_tags: bool
+        :param only_modality: specify which modalities to read in, if not then all modalities will be read
+        :type only_modality: list
+        :param only_load_roi_names: will only load rois with input name, list format
+        :type only_load_roi_names: list
+        """
+        self.exclude_files = exclude_files
+        self.only_tags = only_tags
+        self.only_load_roi_names = only_load_roi_names
+        if only_modality is not None:
+            self.only_modality = only_modality
+        else:
+            self.only_modality = ['CT', 'MR', 'PT', 'US', 'DX', 'MG', 'NM', 'XA', 'CR', 'RTSTRUCT', 'REG', 'RTDose']
+
         if folder_path is not None:
-            self.files = file_parsar(folder_path, exclude_files=exclude_files)
+            self.files = file_parsar(folder_path, exclude_files=self.exclude_files)
         else:
             self.files = file_list
 
-        self.images = None
-        self.rigid = None
-        self.deformable = None
-        self.pois = None
-        self.dose = None
+        self.images = []
+        self.rigid = []
+        self.deformable = []
+        self.dose = []
 
-    def read_all(self, dcm=None, mhd=None, stl=None, mf3=None):
-        if dcm is not None:
-            dicom_reader = DicomReader(self.files)
+    def read_all(self, dcm=False, mhd=False, stl=False, mf3=False):
+        if dcm:
+            dicom_reader = DicomReader(self)
+            dicom_reader.load_dicom()
 
-    def read_dicoms(self, only_tags=False, only_load_roi_names=None):
-        if only_tags:
-            print('reader')
-        else:
-            dicom_reader = DicomReader(self.files, only_load_roi_names=only_load_roi_names)
+    def read_dicoms(self):
+        """
+        Reads in all dicom files and separates them into the image list variable.
+        :return:
+        :rtype:
+        """
+        dicom_reader = DicomReader(self)
+        dicom_reader.load_dicoms()
 
     def read_rtstruct_only(self, base_image=None):
         print('reader')
@@ -42,5 +110,5 @@ class Reader:
         print('reader')
 
 
-if __name__ == '__main__':
+if __name__ == '_main__':
     pass
