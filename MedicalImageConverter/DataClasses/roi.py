29bd744ed9b53cd12db88b4f39ccba8c11a5a150
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-01-24 12:15:15 -0600

    Added - conversion from physical space to pixel/voxel space

--- MedicalImageConverter/DataClasses/roi.py
+++ MedicalImageConverter/DataClasses/roi.py
@@ -14,9 +14,11 @@ Structure:
 import copy
 import numpy as np
 
+import SimpleITK as sitk
+
 
 class Roi(object):
-    def __init__(self, image, name=None, color=None, visible=None, filepaths=None):
+    def __init__(self, image, position=None, name=None, color=None, visible=None, filepaths=None):
         self.image = image
 
         self.name = name
@@ -24,11 +26,25 @@ class Roi(object):
         self.color = color
         self.filepaths = filepaths
 
-        self.contour_position = None
-        self.contour_pixel = None
+        if position is not None:
+            self.contour_position = position
+            self.contour_pixel = self.convert_position_to_pixel()
+        else:
+            self.contour_position = None
+            self.contour_pixel = None
+            
         self.mesh = None
         self.display_mesh = None
         self.decimate_mesh = None
         self.mesh_volume = None
         self.mesh_com = None
         self.bounds = None
+
+    def convert_position_to_pixel(self):
+        sitk_image = self.image.create_sitk_image(empty=True)
+
+        pixel = [[]] * len(self.contour_position)
+        for ii, contours in enumerate(self.contour_position):
+            pixel[ii] = [sitk_image.TransformPhysicalPointToContinuousIndex(contour) for contour in contours]
+
+        return pixel
