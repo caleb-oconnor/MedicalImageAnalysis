3abac06ec387f4b1bf9239986a82e678e5fb8bb1
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-02-23 19:08:38 -0600

    Changed - file_parsar is now a function inside reader

--- MedicalImageConverter/reader.py
+++ MedicalImageConverter/reader.py
@@ -24,7 +24,8 @@ Functions:
 
 """
 
-from .parsar import file_parsar
+import os
+
 from .ReadClasses import DicomReader, MhdReader, NiftiReader, StlReader, VtkReader, ThreeMfReader
 
 
@@ -70,10 +71,8 @@ class Reader:
         else:
             self.only_modality = ['CT', 'MR', 'PT', 'US', 'DX', 'MG', 'NM', 'XA', 'CR', 'RTSTRUCT', 'REG', 'RTDose']
 
-        if folder_path is not None:
-            self.files = file_parsar(folder_path, exclude_files=self.exclude_files)
-        else:
-            self.files = file_list
+        self.files = None
+        self.file_parsar(folder_path=folder_path, file_list=file_list, exclude_files=self.exclude_files)
 
         self.images = []
         self.rigid = []
@@ -81,6 +80,85 @@ class Reader:
         self.dose = []
         self.meshes = []
 
+    def file_parsar(self, folder_path=None, file_list=None, exclude_files=None):
+        """
+        Walks through all the subfolders and checks each file extensions. Sorts them into 6 different options:
+            Dicom
+            MHD
+            Raw
+            Nifti
+            VTK
+            STL
+            3mf
+            No extension
+
+        :param folder_path: single folder path
+        :type folder_path: string path
+        :param file_list: list of filepaths
+        :type file_list: list
+        :param exclude_files: list of filepaths
+        :type exclude_files: list
+        :return: dictionary of into files sorted by extensions
+        :rtype: dictionary
+        """
+
+        no_file_extension = []
+        dicom_files = []
+        mhd_files = []
+        raw_files = []
+        nifti_files = []
+        stl_files = []
+        vtk_files = []
+        mf3_files = []
+
+        if not exclude_files:
+            exclude_files = []
+
+        if file_list is None:
+            file_list = []
+            for root, dirs, files in os.walk(folder_path):
+                if files:
+                    for name in files:
+                        file_list += [os.path.join(root, name)]
+
+        for filepath in file_list:
+            if filepath not in exclude_files:
+                filename, file_extension = os.path.splitext(filepath)
+
+                if file_extension == '.dcm':
+                    dicom_files.append(filepath)
+
+                elif file_extension == '.mhd':
+                    mhd_files.append(filepath)
+
+                elif file_extension == '.raw':
+                    raw_files.append(filepath)
+
+                elif file_extension == '.gz':
+                    if filepath[-6:] == 'nii.gz':
+                        nifti_files.append(filepath)
+
+                elif file_extension == '.stl':
+                    stl_files.append(filepath)
+
+                elif file_extension == '.vtk':
+                    vtk_files.append(filepath)
+
+                elif file_extension == '.3mf':
+                    mf3_files.append(filepath)
+
+                elif file_extension == '':
+                    no_file_extension.append(filepath)
+
+        self.files = {'Dicom': dicom_files,
+                      'MHD': mhd_files,
+                      'Raw': raw_files,
+                      'Nifti': nifti_files,
+                      'Stl': stl_files,
+                      'Vtk': vtk_files,
+                      '3mf': mf3_files,
+                      'NoExtension': no_file_extension}
+        
     def read_dicoms(self):
         """
         Reads in all dicom files and separates them into the image list variable.
