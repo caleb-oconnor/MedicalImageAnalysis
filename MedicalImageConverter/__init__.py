15dfbb452fb05639cdaedac17833e1d1c202ef8b
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-03-07 12:01:31 -0600

    Changed - folder name from Analysis to Utils

--- MedicalImageConverter/__init__.py
+++ MedicalImageConverter/__init__.py
@@ -3,4 +3,4 @@ from .reader import Reader
 from .conversion import ContourToDiscreteMesh, ContourToMask, ModelToMask
 from .Read import DicomReader, MhdReader, NiftiReader, StlReader, VtkReader, ThreeMfReader
 from .Data import Image
-from .Analysis import Refinement, Volume
+from .Utils import Refinement, Volume
