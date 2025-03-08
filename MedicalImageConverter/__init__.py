f63cad1de13eab916fea4e08c34fe7907fca2c60
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-03-08 08:46:06 -0600

    Changed - folder name structure

--- MedicalImageConverter/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .reader import Reader
-
-from .Read import DicomReader, MhdReader, NiftiReader, StlReader, VtkReader, ThreeMfReader
-from .Data import Image
-from .Utils import Refinement, Volume, ContourToDiscreteMesh, ContourToMask, ModelToMask
-# from .Utils import (Refinement, Volume, ContourToDiscreteMesh, ContourToMask, ModelToMask, CreateDicomImage,
-#                     CreateImageFromMask)
