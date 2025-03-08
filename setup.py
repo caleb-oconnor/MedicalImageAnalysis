b812c97660d4ee7ef5ae72548c9f23bc60079460
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-03-07 21:08:06 -0600

    Update - setup file

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 __author__ = 'Caleb OConnor'
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+def get_requirements(file):
+    with open(file, 'r') as f:
+        return f.read().splitlines()
+
 
 setup(
     name='MedicalImageConverter',
     author='Caleb OConnor',
     author_email='csoconnor@mdanderson.org',
-    version='2.0.6',
+    version='2.1.71',
     description='Reads in medical images and structures them into 3D arrays with associated ROI/POIs if they exist.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['MedicalImageConverter', 'MedicalImageConverter/DataClasses', 'MedicalImageConverter/ReadClasses'],
+    packages=find_packages(),
     include_package_data=True,
     url='https://github.com/caleb-oconnor/MedicalImageConverter',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3",
     ],
-    install_requires=['numpy', 'pandas', 'psutil', 'pydicom', 'pyvista', 'python-gdcm', 'opencv-python', 'SimpleITK'],
+    install_requires=get_requirements('requirements.txt'),
 )
