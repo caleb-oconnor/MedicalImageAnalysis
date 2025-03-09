556c0a6c3dce5c66869d6166027d2f444c96aac3
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-03-09 16:30:12 -0500

    Added - new data class option

--- medicalimageanalysis/structure/rigid.py
+++ medicalimageanalysis/structure/rigid.py
@@ -14,18 +14,22 @@ Structure:
 import numpy as np
 
 from ..utils.rigid.icp import IcpOpen3d
+from ..data import Data
 
 
-class Rigid(object):
-    def __init__(self, source_name, target_name, roi_names):
+class Rigid(Data):
+    def __init__(self, source_name=None, target_name=None, roi_names=None):
 
-        self.reference_name = None
+        self.source_name = None
         self.target_name = None
         self.roi_names = roi_names
 
         self.matrix = np.identity(4)
         self.combo_matrix = np.identity(4)
         self.combo_name = None
+
+    def loaded_rigid(self):
+        self.roi_names = ['Unknown']
 
     def compute_icp(self, algorithm='vtk'):
         if algorithm == 'vtk':
