910e6e0942099b221a816f522bb8d057ebb687fe
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-03-09 16:13:58 -0500

    Added - data class to store structures

--- medicalimageanalysis/__init__.py
+++ medicalimageanalysis/__init__.py
@@ -1,6 +1,6 @@
 
-from .reader import Reader
+from medicalimageanalysis.reader import Reader
 
-from .read import DicomReader, MhdReader, NiftiReader, StlReader, VtkReader, ThreeMfReader
-from .data import Image
-from .utils import Refinement, Volume, ContourToDiscreteMesh, ContourToMask, ModelToMask
+from .data import Data
+
+Data()
