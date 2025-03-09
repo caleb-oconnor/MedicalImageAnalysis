d29a5accedd0326c7140045539e79c609fd40e08
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-03-09 18:11:32 -0500

    Added - deformable set function

--- medicalimageanalysis/structure/deformable.py
+++ medicalimageanalysis/structure/deformable.py
@@ -15,9 +15,13 @@ import numpy as np
 
 
 class Deformable(object):
-    def __init__(self, images):
-        self.images = images
+    def __init__(self, source_name=None, target_name=None, roi_names=None):
 
-        self.reference_name = None
+        self.source_name = None
         self.target_name = None
+
         self.matrix = np.identity(4)
+        self.dvf = None
+
+    def add_deformable(self):
+        Data.deformable += [self]
