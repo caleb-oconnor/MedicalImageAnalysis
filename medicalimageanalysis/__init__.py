8ee5569195b17f5b9f1e3f8ef1e65ff3e4c3f668
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-03-09 18:11:55 -0500

    Added - import classes

--- medicalimageanalysis/__init__.py
+++ medicalimageanalysis/__init__.py
@@ -2,7 +2,7 @@
 from .reader import Reader
 
 from .read import DicomReader, MhdReader, NiftiReader, StlReader, VtkReader, ThreeMfReader
+from .structure import Rigid, Deformable
 
 from .data import Data
-
 Data()
